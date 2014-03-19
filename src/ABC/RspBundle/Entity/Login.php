941a2ee30d7eb9fd07e1b3038621d83a7f3b3eb8
Farazath <mefarazath@gmail.com>
2014-03-19 22:48:17 +0530

    Style Bundle, Admin Bundles created

---
+++ src/ABC/RspBundle/Entity/Login.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace ABC\RspBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Login
+ */
+class Login
+{
+    /**
+     * @var string
+     */
+    private $password;
+
+    /**
+     * @var string
+     */
+    private $type;
+
+    /**
+     * @var string
+     */
+    private $username;
+
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     * @return Login
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string 
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     * @return Login
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+    
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string 
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string 
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+}
