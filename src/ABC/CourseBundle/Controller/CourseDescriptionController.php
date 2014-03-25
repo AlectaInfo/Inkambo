1c889d0f75b494f06b776fb86dd899e9985bc108
Farazath <mefarazath@gmail.com>
2014-03-25 15:26:46 +0530

    hassa merging

--- src/ABC/CourseBundle/Controller/CourseDescriptionController.php
+++ src/ABC/CourseBundle/Controller/CourseDescriptionController.php
@@ -1,8 +1,31 @@
 <?php
+//CourseDescription
+namespace ABC\CourseBundle\Controller;
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+//use Symfony\Component\HttpFoundation\Request;
 
+class CourseDescriptionController extends Controller {
+    public function ViewCourseAction(){
+        $em = $this->getDoctrine()->getEntityManager();
+        $repository1 = $em->getRepository('ABCCourseBundle:Course');
+    //    $repository2 = $em->getRepository('ABCCourseBundle:Session');
+
+        $courses = $repository1->findAll();
+        
+        $view_courses_array;
+        
+        foreach($courses as $course){
+            $stmt = $em
+                ->getConnection()
+                ->prepare("SELECT * FROM view_courses where course_id =". "'".$course->getCourseId()."' and year = '2014'"   );
+        $stmt->execute();
+        $result = $stmt->fetchAll();
+        $view_courses_array[$course->getCourseId()] = $result;
+         //   $sessions = $repository2->findBy(array('course' => $course->getCourseId()));
+         //   $session_array[$course->getCourseId()] = array($session); 
+        }
+        
+        return $this->render('ABCCourseBundle:viewcourses:courseDescription.html.twig', array('courses' => $courses, 'all_courses' => $view_courses_array));
+    }
+}
